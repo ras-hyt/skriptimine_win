0c0007e81726bbe1a1de4b96eadb17a1bebaac4a
Rasmus Hytönen <rasmus.hytonen@voco.ee>
2022-11-16 10:36:19 +0200

    Kasutaja kustutamine - yl2

---
+++ kasutaja_kustutamine.ps1
@@ -0,0 +1,12 @@
+﻿#vaatab kas error on
+try {
+#Küsime konto info
+$Eesnimi = Read-Host -Prompt "Sisesta oma eesnimi"
+$Perenimi = Read-Host -Prompt "Sisesta oma Perenimi"
+$Kasutajanimi = $Eesnimi + "." + $Perenimi
+Write-Output "Kasutaja $($Kasutajanimi.ToLower()) on kustutatud" 
+
+#Kustutame konto
+Remove-LocalUser $Kasutajanimi
+}
+Catch{Write-Host "Midagi läks valesti"}
